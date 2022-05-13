904be37eb0b5a1e1f56400d692c237c745443dd4
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2022-05-13 17:39:24 +0300

    "adding setup"

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='PrettyPrintTree',
-    version='1.0',
+    version='1.2',
     author="Aharon Sambol",
     author_email='email@example.com',
-    packages=find_packages('prettyprinttree'),
+    py_modules=find_packages('prettyprinttree'),
     package_dir={'': 'prettyprinttree'},
     url='https://github.com/AharonSambol/PrettyPrintTree',
     keywords=['tree', 'pretty', 'print', 'pretty-print', 'display'],
