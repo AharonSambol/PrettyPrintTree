e776ecd90ef249ba279e057c02f71c01caed8a43
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2022-05-14 23:08:33 +0300

    "trying to fix pypi"

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='PrettyPrintTree',
-    version='1.17',
+    version='1.18',
     author="Aharon Sambol",
     author_email='email@example.com',
     py_modules=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),
+    license='MIT License',
     url='https://github.com/AharonSambol/PrettyPrintTree',
     keywords=['tree', 'pretty', 'print', 'pretty-print', 'display'],
     description='A tool to print trees to the console',
