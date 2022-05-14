3df0571f4b82a63e498418d1ea85138afd035336
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2022-05-14 22:54:48 +0300

    "trying to fix pypi"

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='PrettyPrintTree',
-    version='1.14',
+    version='1.15',
     author="Aharon Sambol",
     author_email='email@example.com',
     py_modules=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/AharonSambol/PrettyPrintTree',
     keywords=['tree', 'pretty', 'print', 'pretty-print', 'display'],
-    description='A tool to print trees to the console'
+    description='A tool to print trees to the console',
+    long_description='A tool to print trees to the console'
 )
