e7573e87c6148e807eed56a79499843813f74994
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2022-05-15 00:21:12 +0300

    "trying to fix pypi"

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='PrettyPrintTree',
-    version='1.192',
+    version='1.3',
     author="Aharon Sambol",
     author_email='email@example.com',
     py_modules=find_packages('PrettyPrint'),
