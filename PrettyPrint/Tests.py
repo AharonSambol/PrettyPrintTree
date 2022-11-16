c558233799bf7cc2ca304f85eb91902baa84ff02
Mark Mayo <mark@there.co.nz>
2022-11-17 11:57:01 +1300

    some cleanups
    while playing with it, ran black, isort and cleaned some pylint/flake8 changes. Hope it's useful.

--- PrettyPrint/Tests.py
+++ PrettyPrint/Tests.py
@@ -1,5 +1,6 @@
-from PrettyPrint import PrettyPrintTree
 from colorama import Back
+
+from PrettyPrint import PrettyPrintTree
 
 
 class Tree:
@@ -19,9 +20,9 @@ class Person:
         self.age = age
 
     def __str__(self):
-        return f"""Person {{ 
-    age: {self.age}, 
-    name: {self.name} 
+        return f"""Person {{
+    age: {self.age},
+    name: {self.name}
 }}"""
 
 
@@ -73,4 +74,3 @@ some_json = {'foo': 1, 'bar': (('a', 'a2'), 'b'), 'qux': {'foo': 1, 'arr': [{1: 
 PrettyPrintTree(color=Back.WHITE)(some_json)
 print()
 PrettyPrintTree().print_json(some_json, name="DICT", max_depth=3)
-
